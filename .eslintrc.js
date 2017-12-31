8f8215694143b06a1b584a0b0545648c37db4822
Ben Lenser <ben.lenser@immobilienscout24.de>
2017-12-31 14:10:23 +0100

    Change no-extraneous-dependencies linting error to warn (at first)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ],
-    "globals": {
-        "document": true
-    },
-    rules:{
-        "import/no-extraneous-dependencies": ["error", {"devDependencies": ["**/browser/**/*","**/*.spec.js"]}],
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    }
+  extends: 'airbnb',
+  plugins: [
+    'react',
+    'jsx-a11y',
+    'import',
+  ],
+  globals: {
+    document: true,
+  },
+  rules: {
+    'import/no-extraneous-dependencies': ['warn', { devDependencies: ['**/browser/**/*', '**/*.spec.js'] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
 };
